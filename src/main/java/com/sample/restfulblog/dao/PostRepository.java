bfdea31815ac2ce1768e5f1eb9ebf59a206d0c33
Benj Sicam <info@benjsicam.me>
2013-05-11 10:15:16 +0800

    Added findPostAuthor query for Author retrieval

--- src/main/java/com/sample/restfulblog/dao/PostRepository.java
+++ src/main/java/com/sample/restfulblog/dao/PostRepository.java
@@ -18,4 +18,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 	
 	@Query("SELECT post.id, post.date, post.content FROM Post post JOIN post.categories category WHERE category.id=?1")
 	List<Post> findPostsByCategoryId(Long id);
+	
+	@Query("SELECT post.author FROM Post post WHERE post.id=?1")
+	Author findPostAuthor(Long id);
 }
